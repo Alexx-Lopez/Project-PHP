5b89da90403cedad8ad4ff696d964dcbb8eb933b
Alexx <eralexxnoyi@gmail.com>
2018-09-01 23:30:17 -0600

    inicio

---
+++ Proyecto/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>FORMA-TEC</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="CSS/Style.css">
+</head>
+<body>
+  <header>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#"><img src=""></a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Inicio</a></li>
+          <li><a href="#">¿Quienes Somos?</a></li>
+          <li><a href="#">Mision</a></li>
+          <li><a href="#">Vision</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</body>
+</html>
