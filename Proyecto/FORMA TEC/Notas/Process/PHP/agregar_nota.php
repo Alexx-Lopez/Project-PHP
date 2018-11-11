1d4821ee81c75194ef53083cf2e113c436aa0daf
Angel <enk_crown@hotmail.com>
2018-11-10 22:21:19 -0600

    nota crud completo

--- Proyecto/FORMA TEC/Notas/Process/PHP/agregar_nota.php
+++ Proyecto/FORMA TEC/Notas/Process/PHP/agregar_nota.php
@@ -98,7 +98,7 @@ if($bandera==false)
       }
 
       //se procede a realizar la insercion
-      $sql = "INSERT INTO nota VALUES(NULL,'$nota','$id_curso', $id_alumno, $id_resultado)";
+      $sql = "INSERT INTO nota VALUES(NULL,'$nota',NULL, $id_alumno, $id_resultado)";
 
       if ($objeto_con->conexion->query($sql) === TRUE) {
         echo "
